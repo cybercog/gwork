1aa6e7481f9530a112a2d1a5f55b1088c756d6b0
Angel Guevara <aguevara@tecnocen.com>
2015-01-16 21:12:56 -0600

    gii generated default controller

---
+++ controllers/DefaultController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Faryshta\gwork\controllers;
+
+use yii\web\Controller;
+
+class DefaultController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
